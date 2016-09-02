9a2c44317ec2b77562ab029d0e5be5c7d44ec9a4
Bear <bear@VanadiumBook.local>
2016-09-02 13:56:58 -0400

    Attempt - make App into multiple pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def first
+
+  end
+
+  def second
+    
+
+  end
+  def show
+    render template: "welcome/#{params[:page]}"
+  end
 end
