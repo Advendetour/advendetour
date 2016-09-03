6495fd2e17c3193dc735299aa5deb0544ebb48f3
Paige Sun <paige.sun@cs.mcgill.ca>
2016-09-03 11:06:18 -0400

    Sample empty map in map.js. Sample map CSS in welcome.scss. Sample API call in welcome_controller.rb, which passes the result to index.html.rb.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+
+    #@result = Net::HTTP.get(URI.parse('http://www.site.com/about.html'))
+
+    @result = Net::HTTP.get(URI.parse('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyCTaQ8rj5xULxzLfTnu5cgvNSo5lxw2YRA'))
+
+    JSON.parse(@result).each do |item|
+      puts item
+      #Rails.logger.info item['id']
+    end
+
+
   end
 end
